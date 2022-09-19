c770c2e0e406f1a33b8dff8eebea085d721314f5
Adarsh TS <tsadarsh0707@gmail.com>
2022-09-19 12:29:03 +0530

    refactor drive code - added publisher for motor commands

--- wroom/wroom/joy_to_drive.py
+++ wroom/wroom/joy_to_drive.py
@@ -1,30 +1,49 @@
-from math import sqrt, degrees, atan
+# Generic ROS imports
 import serial
 import struct
 import rclpy
 from rclpy.node import Node
-from numpy import interp
-import numpy as np
 
-from std_msgs.msg import Int16
+# ROS messages imports
+from wroom.msg import sabertooth_simplified_serial as sss
 from sensor_msgs.msg import Joy
 
+# Local imports
+import DRIVE_TRANSFORM_INTERPOLATE as DTI
 
-def lerp( x, min_in, max_in, min_out, max_out ):
-	return min_out + (x - min_in) * (max_out-min_out)/(max_in-min_in)
 
-class JoySubscriber(Node):
+class JoyToDrive(Node):
+	"""Initialise node that subscribes to /joy topic and sends the joy 
+	values to DTI.
+	"""
 
 	def __init__(self):
+		motor_cmd = [] # unit8 array
+
 		super().__init__ ('joy_subscriber')
 		self.ser = 0
 		self.start_serial()
 		self.subscription = self.create_subscription(
 			Joy,
 			'joy',
-			self.listener_callback,
+			self.DTI_caller,
 			10)
 		self.subscription
+
+		# Publisher to publish motor commands 
+		self.publisher_ = self.create_publisher(sss)
+		timer_period = 0.5 # seconds
+		self.timer = self.create_timer(timer_period, self.timer_callback)
+
+	def DTI_caller(self, joy):
+		"""Sends joy input to DTI and logs returned values"""
+		motor_cmd = DTI.simple_alpha_beta(joy)
+		self.get_logger().info("{motor_cmd}")
+
+	def timer_callback(self):
+		msg = sss()
+		msg.cmd = self.motor_cmd
+		self.publisher_.publish(msg)
 
 	def start_serial(self):
 		try:
@@ -34,90 +53,25 @@ class JoySubscriber(Node):
 		except:
 			self.get_logger().info("Serial COM unsuccessful")
 
-	def bezier_interp(self, x, limits, delta):
-		reverse, neutral, forward = np.array([
-			[-2, limits[0]],
-			[0, (sum(limits)/len(limits)) + delta],
-			[2, limits[1]]
-			])
-		P = lambda t: (1-t)**2 * reverse[1] + 2*t*(1-t) * neutral[1] + t**2 * forward[1]
-		return P(x)
-
-	def lazy_ramp(self, x, y, acc):
-		alpha = y + x
-		beta = y - x
-		alpha_unit = interp(alpha, [-2, 2], [-1, 1])
-		beta_unit = interp(beta, [-2, 2], [-1, 1])
-		acc = interp(acc, [-1, 1], [1, 10])
-		alpha_final = alpha ** acc
-		self.publish_serial(alpha_final, beta_unit)
-
-
-
-	def listener_callback(self, joy):
-		#self.calculate_alpha_beta(-joy.axes[0], joy.axes[1])
-		self.lazy_ramp(-joy.axes[0], joy.axes[1], joy.axes[3])
-
-	def simple_alpha_beta(self, x, y, max_speed):
-		alpha = y+x
-		beta = y-x
-		xp = interp(max_speed, [-1,1], [-10,10])
-		alpha_lerp = self.bezier_interp(alpha, [127, 1], max_speed)
-		beta_lerp = self.bezier_interp(beta, [128, 255], max_speed)
-		self.publish_serial(alpha_lerp, beta_lerp)
-
-	def harmonic_alpha_beta(alpha, beta):
-			xp, yp = [1, 127], [128, 255]
-			alpha_lerp = interp(alpha, [-1, 1], xp)
-			beta_lerp = interp(beta, [-1, 1], yp)
-			self.publish_serial(alpha_lerp, beta_lerp)
-
-	def calculate_alpha_beta(self, x, y, delta):
-		if x==0 and y==0:
-			# joystick in home position
-			alpha, beta = 0, 0
-		elif x == 0:
-			# joy in only forward or backward
-			alpha, beta = y, y
-
-		elif y == 0:
-			alpha, beta = y, -y
-
-		elif x > 0 and y > 0:
-			# Quad 1
-			alpha = sqrt(x**2 + y**2)
-			beta = (y**2 - x**2)/(y**2 + x**2)
-
-		elif x < 0 and y > 0:
-			# Quad 2
-			alpha = (y**2 - x**2)/(y**2 + x**2)
-			beta = sqrt(x**2 + y**2)
-
-		elif x < 0 and y < 0:
-			# Quad 3
-			alpha = -((y**2 - x**2)/(y**2 + x**2))
-			beta = -(sqrt(x**2 + y**2))
-
-		elif x > 0 and y < 0:
-			alpha = -(sqrt(x**2 + y**2))
-			beta = -((y**2 - x**2)/(y**2 + x**2))
-
-		else:
-			self.get_logger().info(f"unmapped data: {x}, {y}")
-		self.get_logger().info(f'data: {alpha}, {beta}')
-
-		if self.ser:
-			self.lazy_ramp(alpha, beta)
-
 	def publish_serial(self, alpha, beta):
 		self.ser.write(struct.pack('>B', int(alpha)))
 		self.ser.write(struct.pack('>B', int(beta)))
 		self.get_logger().info(f'data: {alpha}, {beta}')
 
+class MotorDriverCMD(Node):
+
+	def __init__(self):
+		super().__init__('motor_driver_cmd')
+		self.publisher_ = self.create_publisher(sss)
+		timer_period = 0.5 # seconds
+		self.timer = self.create_timer(timer_period, self.timer_callback)
+
+
 def main(args=None):
 	rclpy.init(args=args)
 
 	joy_subscriber = JoySubscriber()
+	motor_driver_cmd = MotorDriverCMD()
 
 	rclpy.spin(joy_subscriber)
 
